67a9d5dbe50b41d4a9f59cd115fc721c00e29e3a
rishabh27121998 <rishabhjain506@gmail.com>
2021-03-26 17:44:25 +0530

    Second Commit

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main()
 {
 
     for(int i=1;i<=5;i++)
     {
-
         for(int j=1;j<=9;j++)
         {
             if(j<=6-i||j>=4+i)
